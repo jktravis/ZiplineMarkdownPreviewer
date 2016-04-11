8bbf2b57c9125b79f84ecb1a370587670b40da9f
jktravis <joshua.travis@gmail.com>
2016-04-11 19:09:01 -0400

    Moved initial state values to placeholder.

--- src/js/components/entry/entryPane.js
+++ src/js/components/entry/entryPane.js
@@ -5,13 +5,13 @@ var React = require('react');
 var EntryPane = React.createClass({
   getInitialState: function getInitialState() {
     return {
-      input: 'This is an Entry pane.'
+      input: 'Please entry your Markdown.'
     };
   },
   render: function render() {
     return (
       <div className="entryPane">
-        <textarea className="form-control">{this.state.input}</textarea>
+        <textarea className="form-control" placeholder={this.state.input}></textarea>
       </div>
     );
   }
