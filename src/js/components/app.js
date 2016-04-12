d8cefd55e03fd4e2f776e741cac0c46cbc772035
jktravis <joshua.travis@gmail.com>
2016-04-11 20:43:37 -0400

    Added some styling.

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -19,13 +19,15 @@ var App = React.createClass({
       <div>
         <div className="container-fluid">
           <div className="row">
-            <h1>Markdown Previewer</h1>
+            <header>
+              <h1 className="text-primary">Markdown Previewer</h1>
+            </header>
           </div>
           <div className="row">
-            <div className="col-lg-5 col-lg-offset-1">
+            <div className="col-lg-5 col-lg-offset-1 col-md-6 col-sm-6 col-xs-6">
               <EntryPane onChange={this.setMarkdownState}/>
             </div>
-            <div className="col-lg-5">
+            <div className="col-lg-5 col-md-6 col-sm-6 col-xs-6">
               <PreviewPane markdown={this.state.markdown}/>
             </div>
           </div>
